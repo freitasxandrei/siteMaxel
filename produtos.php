c803363cefe7e69298c687ffc480c132eb79fc03
Andrei de Freitas <freitasxandrei@gmail.com>
2022-05-24 21:06:35 -0300

    beeeee

--- produtos.php
+++ produtos.php
@@ -12,47 +12,74 @@
 
   <title> FREITASXANDREI </title>
 
-<style>
+  <style>
+    body {
+      background-color: #C7A4A4;
+    }
 
-body {
-    background-color: #C7A4A4;
-}
+    .navbar1 {
+      position: relative;
+      display: -ms-flexbox;
+      display: flex;
+      -ms-flex-wrap: wrap;
+      flex-wrap: wrap;
+      -ms-flex-align: center;
+      align-items: center;
+      -ms-flex-pack: justify;
+      justify-content: center;
+      padding: 0.5rem 1rem;
+      border-style: solid;
+      border-width: 3px;
+      border-radius: 25px;
+      margin: 8% 10%;
+      box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    }
 
-.navbar1 {
-    position: relative;
-    display: -ms-flexbox;
-    display: flex;
-    -ms-flex-wrap: wrap;
-    flex-wrap: wrap;
-    -ms-flex-align: center;
-    align-items: center;
-    -ms-flex-pack: justify;
-    justify-content: center;
-    padding: 0.5rem 1rem;
-    border-style: solid;
-    border-width: 3px;
-    border-radius: 25px;
-    margin: 8% 10%;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-}
-.imagemlogo {
-    width: 20%;
-    height: 20%;
-    border-style: solid;
-    border-width: 3px;
-    border-radius: 25px;
-    border-color: black;
+    .imagemlogo {
+      width: 20%;
+      height: 20%;
+      border-style: solid;
+      border-width: 3px;
+      border-radius: 25px;
+      border-color: black;
 
-}
-.container1 {
-    background-color: black;
-    border-style: solid;
-    border-width: 3px;
-    border-radius: 25px;
-    border-color: black;
-}
+    }
 
-</style>
+    .container1 {
+      background-color: black;
+      border-style: solid;
+      border-width: 3px;
+      border-radius: 25px;
+      border-color: black;
+    }
+
+    .navbarmaxel {
+      text-align: center;
+    }
+
+    .iconesmaxel {
+      background-color: black;
+      border-style: solid;
+      border-width: 3px;
+      border-radius: 100%;
+      border-color: black;
+    }
+    .rowandrei {
+      margin: 10% 10%;
+    }
+    .imageandrei {
+      border-style: solid;
+      border-width: 3px;
+      border-radius: 25px;
+      border-color: black;
+    }
+    .jumbandrei {
+      border-style: solid;
+      border-width: 3px;
+      border-radius: 25px;
+      border-color: black;
+    }
+  </style>
 
 </head>
 
@@ -63,63 +90,85 @@ body {
       <p class="lead"> Registro de Ninjas! </p>
     </header>
 
+    <nav class="navbar1 navbar-light bg-light">
+      <span class="navbar-brand mb-0 h1"> <b> HOME! </b> </span>
+    </nav>
 
-<nav class="navbar1 navbar-light bg-light">
-  <span class="navbar-brand mb-0 h1"> <b> HOME! </b> </span>
-</nav>
+    <div class="container1">
+      <div class="media text-light">
+        <div class="media-body">
+          <h5 class="mt-4 mb-4" style="text-align: center;"> POR QUE SOMOS OS MELHORES! </h5>
+          <p style="text-align: center;"> A Maxel atua desde 1993 no mercado de materiais elétricos, são mais de 200 colaboradores, uma área superior a 5.000m2. Todos os processos são 100% informatizados, administrando mais de 140.000 itens das mais conceituadas marcas. Seguramente um dos maiores estoques do mercado. </p>
+          <p style="text-align: center;"> Atuamos nos seguimentos da Indústria, Construção Civil, Instaladores, Órgãos Públicos e consumidores finais de todo o Brasil. Oferecemos atendimento através de contrato: “Just in Time”, Consignação e loja “In Company”. </p>
+        </div>
+        <img src="assets/image/maxel.jpg" class="imagemlogo" alt="...">
+      </div>
+    </div>
 
-<div class="container1">
-<div class="media text-light">
-  <div class="media-body">
-    <h5 class="mt-3 mb-4"> POR QUE SOMOS OS MELHORES! </h5>
-    <p> A Maxel atua desde 1993 no mercado de materiais elétricos, são mais de 200 colaboradores, uma área superior a 5.000m2. Todos os processos são 100% informatizados, administrando mais de 140.000 itens das mais conceituadas marcas. Seguramente um dos maiores estoques do mercado. </p>
-<p> Atuamos nos seguimentos da Indústria, Construção Civil, Instaladores, Órgãos Públicos e consumidores finais de todo o Brasil. Oferecemos atendimento através de contrato: “Just in Time”, Consignação e loja “In Company”. </p>    
-</div>
-  <img src="assets/image/maxel.jpg" class="imagemlogo" alt="...">
-</div>
-</div>
-
-<footer class="pt-4 my-md-5 pt-md-5 border-top">
-  <div class="row andrei">
-    <div class="col-3">
-      <img class="mb-2" src="assets/image/perao.jpg" width="80" height="80">
-      <small class="d-block mb-3 text-muted">© 2022-2023</small>
+    <div class="container">
+  <div class="row rowandrei">
+    <div class="col">
+     <input type="image" class="iconesmaxel" src="assets/image/loja.jpg" width="120px" height="120px">  
     </div>
-    <div class="col-3">
-      <h5 class="andreifooter"> MAIS </h5>
-      <ul class="list-unstyled text-small">
-        <li><a class="text-muted" href="#">Algo legal</a></li>
-        <li><a class="text-muted" href="#">Feature aleatória</a></li>
-        <li><a class="text-muted" href="#">Recursos para times</a></li>
-        <li><a class="text-muted" href="#">Coisas para desenvolvedores</a></li>
-        <li><a class="text-muted" href="#">Outra coisa legal</a></li>
-        <li><a class="text-muted" href="#">Último item</a></li>
-      </ul>
+    <div class="col">
+     <input type="image" class="iconesmaxel" src="assets/image/local.jpg" width="120px" height="120px">  
     </div>
-    <div class="col-3">
-      <h5 class="andreifooter"> FONTES </h5>
-      <ul class="list-unstyled text-small">
-        <li><a class="text-muted" href="#">Fonte</a></li>
-        <li><a class="text-muted" href="#">Nome da fonte</a></li>
-        <li><a class="text-muted" href="#">Outra fonte</a></li>
-        <li><a class="text-muted" href="#">Fonte final</a></li>
-      </ul>
+    <div class="col">
+     <input type="image" class="iconesmaxel" src="assets/image/relampago.jpg" width="120px" height="120px">  
     </div>
-    <div class="col-3">
-      <h5 class="andreifooter"> SOBRE </h5>
-      <ul class="list-unstyled text-small">
-        <li><a class="text-muted" href="#">Equipe</a></li>
-        <li><a class="text-muted" href="#">Locais</a></li>
-        <li><a class="text-muted" href="#">Privacidade</a></li>
-        <li><a class="text-muted" href="#">Termos</a></li>
-      </ul>
+    <div class="col">
+     <input type="image" class="iconesmaxel" src="assets/image/pc.jpg" width="120px" height="120px">  
     </div>
   </div>
-</footer>
+
+  <div class="jumbotron jumbotron-fluid jumbandrei" style="box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); background-color: white;">
+  <div class="container" style="text-align: center; margin-top: -3%; margin-bottom: -2%;">
+    <h5 style="margin-bottom:2%; margin-top:-2%;"> <b> NOSSA EQUIPE! </b> </h5>
+    <img class="imageandrei" src="assets/image/tec.jpg" width="80%" height="80%" style="text-align: center;">
 </div>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
-<script src="assets/js/main.js?date=<?php echo time(); ?>"></script>
+</div>
+
+    <footer class=" pt-4 my-md-5 pt-md-5 border-top">
+      <div class="row andrei">
+        <div class="col-3">
+          <img class="mb-2" src="assets/image/perao.jpg" width="80" height="80">
+          <small class="d-block mb-3 text-muted">© 2022-2023</small>
+        </div>
+        <div class="col-3">
+          <h5 class="andreifooter"> MAIS </h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="#">Algo legal</a></li>
+            <li><a class="text-muted" href="#">Feature aleatória</a></li>
+            <li><a class="text-muted" href="#">Recursos para times</a></li>
+            <li><a class="text-muted" href="#">Coisas para desenvolvedores</a></li>
+            <li><a class="text-muted" href="#">Outra coisa legal</a></li>
+            <li><a class="text-muted" href="#">Último item</a></li>
+          </ul>
+        </div>
+        <div class="col-3">
+          <h5 class="andreifooter"> FONTES </h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="#">Fonte</a></li>
+            <li><a class="text-muted" href="#">Nome da fonte</a></li>
+            <li><a class="text-muted" href="#">Outra fonte</a></li>
+            <li><a class="text-muted" href="#">Fonte final</a></li>
+          </ul>
+        </div>
+        <div class="col-3">
+          <h5 class="andreifooter"> SOBRE </h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="#">Equipe</a></li>
+            <li><a class="text-muted" href="#">Locais</a></li>
+            <li><a class="text-muted" href="#">Privacidade</a></li>
+            <li><a class="text-muted" href="#">Termos</a></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+  <script src="assets/js/main.js?date=<?php echo time(); ?>"></script>
 </body>
 
 </html>
